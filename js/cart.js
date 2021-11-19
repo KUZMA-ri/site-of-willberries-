df03f08cca159a8b36ffd27650d8fb7ee1c4ff0c
KUZMA-ri <93029306+KUZMA-ri@users.noreply.github.com>
2021-11-19 22:04:08 +0300

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,20 @@
+const cart = function () {
+    const cartBtn = document.querySelector('.button-cart')
+    const cart = document.querySelector('#modal-cart')
+    const closeBtn = cart.querySelector('.modal-close')
+
+    console.log(cart);
+
+
+    cartBtn.addEventListener('click', function () {
+        cart.style.display = 'flex'
+    })
+
+    closeBtn.addEventListener('click', function () {
+        cart.style.display = ''
+
+})
+   
+}
+
+cart()
